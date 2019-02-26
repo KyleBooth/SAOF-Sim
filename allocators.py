a128a4489320a35456bf1d36cfdd5f723b3988da
Kyle Booth <kbooth@mie.utoronto.ca>
2019-02-25 21:18:20 -0500

    fixes

---
+++ allocators.py
@@ -0,0 +1,30 @@
+## SAOF-Sim: The Simple Autonomous Order Fulfillment Simulator 
+## Author: Kyle E. C. Booth
+## Email: kbooth@mie.utoronto.ca
+## File description: Specify allocation logic. Define our own allocator/scheduler here (with appropriate adjustments in simulator.py). 
+
+import numpy as np
+from allocator_params import *
+
+def allocateRandom(master_queue, available_robots, time): 
+	robot_index = 0
+	assigned = 0
+	task_tracking = [] 
+	assignment = []
+	while robot_index < len(available_robots) and assigned < len(master_queue):
+		task_index = np.random.random_integers(0, len(master_queue) - 1)
+		if (task_index not in task_tracking):
+			task_tracking.append(task_index)
+			assignment.append([available_robots[robot_index].robot_details["ID"], master_queue[task_index].task_details["ID"]])
+			robot_index = robot_index + 1
+			assigned = assigned + 1
+	return assignment
+
+def allocateNaive(master_queue, available_robots, time):
+	assignment_index = 0
+	assignment = []
+	for i in range(len(master_queue)):
+		if assignment_index < len(available_robots):
+			assignment.append([available_robots[assignment_index].robot_details["ID"], master_queue[i].task_details["ID"]])
+		assignment_index = assignment_index + 1
+	return assignment	
---
+++ main.py
@@ -0,0 +1,23 @@
+## SAOF-Sim: The Simple Autonomous Order Fulfillment Simulator 
+## Author: Kyle E. C. Booth
+## Email: kbooth@mie.utoronto.ca
+## File description: Main function. Loads problem/simulation params and calls simulateFactory(). Outputs makespan.
+
+import numpy as np
+import pandas as pd
+import pprint as pp
+import time
+from simulator import *
+
+if __name__ == '__main__':
+
+	## Adjust below parameters for your experiments
+	problem_parameters = {"robot_num": 2, "max_subtasks": 5, "max_tasks": 50, "initial_percentage": 0.2, "horizon": 100, "max_distance": 300, "locations": 15}
+
+	simulation_parameters = {"random_seed": 0, "rate": 1.5, "allocation_method": "random",  "min_available_robots": 1, "max_wait": 0}
+
+	makespan = simulateFactory(problem_parameters, simulation_parameters)
+
+	print ("\n|===|\n(COMPLETE) Execution Makespan: %d \n|===|" % (makespan))
+
+
